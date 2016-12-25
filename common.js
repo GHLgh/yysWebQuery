492e9a7dd9a0c035e3aa36b2d59899ca502be3b6
GHLgh <gluo2@illinois.edu>
2016-12-25 09:50:42 -0600

    implemented basic function
    manipulate based on http://yys.163.com/m/fengyin/
    now it can perform basic query with up to four different kinds of inputs
    TODO: calculate the optimal path for user to finish up to four quests at
    once

---
+++ common.js
@@ -0,0 +1,80 @@
+var isAppInside = /micromessenger/i.test(navigator.userAgent.toLowerCase()) || /yixin/i.test(navigator.userAgent.toLowerCase())
+  , isIos = /iphone os/i.test(navigator.userAgent.toLowerCase()) || /ipad/i.test(navigator.userAgent.toLowerCase())
+  , isAndroid = /android/i.test(navigator.userAgent.toLowerCase());
+var Common = function() {
+    var e = function(e) {
+        $(".alertbox").length > 0 && $(".alertbox").remove(),
+        $(document.body).append('<div class="alertbox" id="alertbox"><div class="alert_text"><a href="javascript:void(0)" class="btn_close"><em></em><i></i></a>' + e + "</div></div>");
+        setTimeout(function() {
+            $("#alertbox").addClass("show")
+        }, 50);
+        $("#alertbox .btn_close").unbind().bind("click", function() {
+            $("#alertbox").addClass("remove");
+            setTimeout(function() {
+                $("#alertbox").remove()
+            }, 600)
+        })
+    };
+    return {
+        alert: e
+    }
+};
+;var _siteConfig = {
+    download: {
+        ios: !0,
+        android: !0
+    }
+};
+var GlobalInit = function() {
+    var n = function(n) {
+        $("html").css("font-size", document.documentElement.clientHeight / document.documentElement.clientWidth < 1.5 ? document.documentElement.clientHeight / 603 * 312.5 + "%" : document.documentElement.clientWidth / 375 * 312.5 + "%"),
+        $("html").css("font-size", document.documentElement.clientWidth / 375 * 312.5 + "%"),
+        n && n()
+    }
+      , i = function() {
+        var n = $("nav ul li")
+          , t = parseFloat(n.eq(0).css("height").replace("px", "")) * n.length;
+        $("nav").attr("data-height", t),
+        $("#btn_nav").bind("click", function() {
+            $(this).addClass("active"),
+            $("nav").show();
+            setTimeout(function() {
+                $("nav ul").css("height", $("nav").attr("data-height") + "px")
+            }, 50)
+        }),
+        $("nav").bind("click", function() {
+            $("#btn_nav").removeClass("active"),
+            $("nav ul").css("height", "0px");
+            setTimeout(function() {
+                $("nav").hide()
+            }, 300)
+        }),
+        $("nav")[0].addEventListener("touchmove", function(n) {
+            n.preventDefault()
+        }, !1)
+    }
+      , o = function() {
+        window.addEventListener("onorientationchange"in window ? "orientationchange" : "resize", function(n) {
+            a(n)
+        }, !1)
+    }
+      , a = function() {
+        if (90 == window.orientation || -90 == window.orientation)
+            $("#forhorview").css("display", "-webkit-box");
+        else {
+            {
+                setTimeout(n, 300)
+            }
+            $("#forhorview").css("display", "none")
+        }
+    }
+      , c = function() {
+        n(function() {
+            var n = isIos ? "ios" : "android";
+            _siteConfig.download[n] ? $(".btn_download").addClass("show") : $(".btn_download").removeClass("show"),
+            o(),
+            $("#btn_nav").length > 0 && i()
+        })
+    };
+    c()
+};
---
+++ query.js
@@ -0,0 +1,209 @@
+/**
+ * 1. Using the code from http://yys.163.com/m/fengyin/ to analyse user input and retrieve information related to the input
+ * 2. Creating a data set based on the retrieved information and computing the optimal path (?) to complete the task (graph maybe??)
+ * 2.1 How to build the graph??
+ */
+
+//TODO: add a layer of indirection between search result and input (user have to click on the target to show detail)
+// step 1: change the way it displays the content
+//  1.1 multiple matches, wait for selecting the right one, display detail => ..., ..., add the title and detail as a string in html format to 
+//      resultList
+//  1.2 exact match, display detail => ..., add the title and detail as a string in html format to resultList
+//
+// step 2: use boots trap to perform detail display
+var resultList = [];
+
+var resultManager = {
+    ifFull: function(){
+        if(resultList.length >= 4){
+            i.dealingRelation($(".OAR-resultError"));
+            $(".OAR-resultError").html("\u6700\u591A\u53EA\u63A5\u53D7\u56DB\u4E2A\u8F93\u5165!");
+            return true;
+        }
+        else
+            return false;
+    },
+    addList: function(nameInput, informationInput){
+        for(var iterator = 0; iterator < resultList.length; iterator++){
+            if(nameInput == resultList[iterator].name){
+                i.dealingRelation($(".OAR-resultError"));
+                $(".OAR-resultError").html("\u8F93\u5165\u5DF2\u67E5\u8BE2\uff0c\u8bf7\u91cd\u65b0\u8f93\u5165!");
+                return;
+            }
+        }
+        var informationPair = {name:nameInput, info:informationInput};
+        resultList.push(informationPair);
+        resultManager.updateDocumentation();
+    },
+    clearList: function(){
+        resultList = new Array;  
+        resultManager.updateDocumentation();
+    },
+    updateDocumentation: function(){
+        for(var i = 0; i < 4; i++){
+            var targetSection = "#list" + i;
+            if(i < resultList.length){
+                $(targetSection).children("li").text(resultList[i].name);
+                $(targetSection).children("div").html(resultList[i].info);
+                $(targetSection).show();
+                
+                $(targetSection).find("a").click(function() {
+                    var idInString = $(this).parent().parent().parent().attr("id");
+                    var id = parseInt(idInString.charAt(idInString.length - 1));
+                    resultList.splice(id,1);
+                    resultManager.updateDocumentation();
+                })
+            }
+            else{
+                $(targetSection).hide();
+            }
+        }
+    }
+};
+
+var i = {
+        dealingRelation: function(i) {
+            i.show().siblings("div").hide()
+        },
+        seek: function() {
+            if(resultManager.ifFull())
+                return -1;
+            var t = $("#input-box").val().trim();
+            return "" == t || 1 == /^\s*$/gi.test(t) ? (i.dealingRelation($(".OAR-resultError")),
+            void $(".OAR-resultError").html("\u8f93\u5165\u7684\u5185\u5bb9\u4e0d\u80fd\u7a7a\uff0c\u8bf7\u91cd\u65b0\u8f93\u5165!")) : void $.ajax({
+                //url: "http://yys.163.com/m/fengyin/json/yys_1027.json",
+                url: "data.json",
+                type: "get",
+                dataType: "json",
+                success: function(s) {
+                    printList(s);
+                    for (var e = 0; e < s.length; e++)
+                        if (t == s[e].name)
+                            return void i.showDetails(s[e]);
+                    for (var n = new Array, d = new Array, l = 0; l < s.length; l++) {
+                        var r = s[l].name;
+                        if (r.indexOf(t) >= 0) {
+                            if (n.indexOf(s[l].ID) >= 0)
+                                continue;
+                            n.push(s[l].ID)
+                        }
+                    }
+                    for (var l = 0; l < s.length; l++)
+                        if (s[l].clue) {
+                            var a = s[l].clue.replace(/\u3001/gi, "");
+                            if (a.indexOf(t) >= 0) {
+                                if (n.indexOf(s[l].ID) >= 0)
+                                    continue;
+                                n.push(s[l].ID)
+                            }
+                        }
+                    for (var e = 0; e < s.length; e++)
+                        if (s[e].othername) {
+                            var u = s[e].othername.replace(/\uff0c/gi, "");
+                            if (u.indexOf(t) >= 0) {
+                                if (n.indexOf(s[e].ID) >= 0)
+                                    continue;
+                                n.push(s[e].ID)
+                            }
+                        }
+                    for (var e = 0; e < n.length; e++)
+                        for (var l = 0; l < s.length; l++)
+                            if (n[e] == s[l].ID) {
+                                d.push(s[l]);
+                                break
+                            }
+                    i.showList(d)
+                },
+                error: function() {
+                    alert("\u7f51\u7edc\u4fe1\u53f7\u4e0d\u597d\uff0c\u8bf7\u5237\u65b0\u518d\u8bd5")
+                }
+            })
+        },
+        showList: function(t) {
+            if (0 == t.length)
+                i.dealingRelation($(".OAR-resultError")),
+                $(".OAR-resultError").html("\u6ca1\u6709\u7b26\u5408\u6761\u4ef6\u7684\u5996\u602a\uff0c\u8bf7\u91cd\u65b0\u8f93\u5165~!");
+            else if (1 == t.length)
+                i.showDetails(t[0]);
+            else {
+                $(".OAR-resultList > ul").html("");
+                for (var s = 0; s < t.length; s++)
+                    $("<li data-id=" + t[s].ID + ">" + t[s].name + "</li>").appendTo($(".OAR-resultList > ul"));
+                i.dealingRelation($(".OAR-resultList")),
+                i.addBind(t)
+            }
+        },
+        addBind: function(t) {
+            $(".OAR-resultList > ul").find("li").each(function() {
+                $(this).click(function() {
+                    for (var s = $(this).data("id"), e = 0; e < t.length; e++)
+                        t[e].ID == s && i.showDetails(t[e])
+                })
+            })
+        },
+        showDetails: function(t) {
+            var s = "";
+            s += '<p class="OAR-resultDetails-title"><span>' + t.name +  '|<a href="javascript:void(0)" id="deleteSelection" style="color:red"> [撤销]</a>' + "</span></p>",
+            s += "<dl><dt>\u7ae0\u8282\uff1a</dt><dd>" + i.isEXist(t.chapter1) + i.isEXist(t.chapter2),
+            s += i.isEXist(t.chapter3) + i.isEXist(t.chapter4) + i.isEXist(t.chapter5),
+            s += i.isEXist(t.chapter6) + i.isEXist(t.chapter7) + i.isEXist(t.chapter8) + i.isEXist(t.chapter9) + i.isEXist(t.chapter10) + "</dd></dl>",
+            s += "<dl><dt>\u7ebf\u7d22\uff1a</dt><dd>" + i.isEXist(t.clue) + "</dd></dl>",
+            s += "<dl><dt>\u5fa1\u9b42\uff1a</dt><dd>" + i.isEXist(t.yuhun1) + i.isEXist(t.yuhun2),
+            s += i.isEXist(t.yuhun3) + i.isEXist(t.yuhun4) + "</dd></dl>",
+            s += "<dl><dt>\u5996\u6c14\u5c01\u5370\uff1a</dt><dd>" + i.isEXist(t.yqfy1) + i.isEXist(t.yqfy2),
+            s += i.isEXist(t.yqfy3) + i.isEXist(t.yqfy4) + i.isEXist(t.yqfy5),
+            s += "</dd></dl>",
+            s += "<dl><dt>\u9b3c\u738b\u5c01\u5370\uff1a</dt><dd>" + i.isEXist(t.gwfy1) + i.isEXist(t.gwfy2),
+            s += i.isEXist(t.gwfy3) + i.isEXist(t.gwfy4) + i.isEXist(t.gwfy5),
+            s += i.isEXist(t.gwfy6) + "</dd></dl>",
+            s += "<dl><dt>\u63a8\u8350\u526f\u672c\uff1a</dt><dd>" + i.isEXist(t.advice) + "</dd></dl>",
+            s += "<dl><dt>\u4f53\u529b\u6d88\u8017\uff1a</dt><dd>" + i.isEXist(t.sushi) + "</dd></dl>";
+            var e = i.markKey(s);
+            $("#testID").html(e),
+            //#testID should never be shown
+            //i.dealingRelation($("#testID")),
+            $(".OAR-resultList").hide(),
+            $(".OAR-column-wrap").show(),
+            i.isNull($("#testID")),
+            resultManager.addList(t.name, $("#testID").html());
+        },
+        isEXist: function(i) {
+            if (i && null != i) {
+                var t = "<p>" + i + "</p>";
+                return t
+            }
+            return ""
+        },
+        isNull: function(i) {
+            i.find("dd").each(function() {
+                "" == $(this).html() && $(this).html("<p>\u65e0</p>")
+            })
+        },
+        markKey: function(i) {
+            var t = /\uff08(.*?)\uff09/g
+              , s = i.replace(t, '<font color="#ca1545">($1)</font>');
+            return s
+        },
+        bindEvent: function() {
+            $("#seek-btn").click(function() {
+                i.seek();
+            })
+            $("#reset-btn").click(function(){
+                resultManager.clearList();
+            })
+        },
+        init: function() {
+            var i = this;
+            i.bindEvent();
+        }
+    };
+
+function printList(s){
+    console.log(s);
+}
+
+function startup(){
+    GlobalInit();
+    i.init();
+    resultManager.updateDocumentation();
+}
